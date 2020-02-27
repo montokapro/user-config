d9d5e5286fbe5872c9bc640fc51b1ba8c727830d
Stephen Webber <montokapro@gmail.com>
2020-02-26 18:59:49 -0600

    pinentry

--- manifests/other.scm
+++ manifests/other.scm
@@ -1,1 +1,1 @@
-(specifications->manifest '("vim" "autoconf" "automake" "gettext" "texinfo" "pkg-config" "keepassxc" "gnupg" "nfs-utils" "hexchat" "cedille" "gtksourceview" "racket" "emacs-geiser" "emacs-guix" "emacs-racket-mode" "libreoffice" "inkscape" "icecat"))
+(specifications->manifest '("vim" "autoconf" "automake" "gettext" "texinfo" "pkg-config" "keepassxc" "gnupg" "pinentry-tty" "nfs-utils" "hexchat" "cedille" "gtksourceview" "racket" "emacs-geiser" "emacs-guix" "emacs-racket-mode" "libreoffice" "inkscape" "icecat"))
