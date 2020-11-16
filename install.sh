b49e72f4f4f552efda0758ea5b886008cc06a078
Stephen Webber <montokapro@gmail.com>
2020-11-15 21:08:32 -0600

    More closely match home directory format

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ ln -rs .bash_profile $HOME/.bash_profile
 ln -rs .bashrc $HOME/.bashrc
 ln -rs .inputrc $HOME/.inputrc
 ln -rs .emacs.el $HOME/.emacs.el
-ln -rs channels.scm $HOME/.config/guix/channels.scm
+ln -rs .config/guix/channels.scm $HOME/.config/guix/channels.scm
