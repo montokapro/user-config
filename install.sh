ee66007b92339706b88f26578fc831a132e09289
Stephen Webber <montokapro@gmail.com>
2020-11-15 20:48:45 -0600

    inputrc

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 ln -rs .bash_profile $HOME/.bash_profile
 ln -rs .bashrc $HOME/.bashrc
+ln -rs .inputrc $HOME/.inputrc
 ln -rs .emacs.el $HOME/.emacs.el
 ln -rs channels.scm $HOME/.config/guix/channels.scm
