b359058ceea0787d38dd103f4babac41466c9a7d
Stephen Webber <montokapro@gmail.com>
2020-11-16 22:44:09 -0600

    move channels.scm

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,4 +1,9 @@
-(cons (channel
-       (name 'montokapro-free)
-       (url "https://github.com/montokapro/guix-free.git"))
-      %default-channels)
+(cons*
+ (channel
+  (name 'montokapro-free-local)
+  (url "file:///home/steve/code/guix/free/.git")
+  (branch "emacs-helm-projectile"))
+ (channel
+  (name 'montokapro-free)
+  (url "https://github.com/montokapro/guix-free.git"))
+ %default-channels)
