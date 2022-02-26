04e65c04d0403e01903d1ac400c98c0fdc018e87
Stephen Webber <montokapro@gmail.com>
2022-02-26 11:43:13 -0600

    git config

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ln -rs .bash_profile $HOME/.bash_profile
 ln -rs .bashrc $HOME/.bashrc
+ln -rs .gitconfig $HOME/.gitconfig
 ln -rs .inputrc $HOME/.inputrc
 ln -rs .emacs.el $HOME/.emacs.el
 ln -rs .config/guix/channels.scm $HOME/.config/guix/channels.scm
